52284376fd557aac13eef10eda2bd71131cbae85
Sandy Carter <bwrsandman@gmail.com>
2013-08-03 11:57:23 -0400

    Update cm.mk to current cyanogen structure cm -> cyanogen boot animation Release name misc

--- cm.mk
+++ cm.mk
@@ -1,18 +1,29 @@
-## Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
 
-# Release name
-PRODUCT_RELEASE_NAME := roamer
+# Inherit some common cyanogenmod stuff.
+$(call inherit-product, vendor/cyanogen/products/common_full.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Include GSM stuff
+$(call inherit-product, vendor/cyanogen/products/gsm.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/zte/roamer/device_roamer.mk)
 
-## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := roamer
-PRODUCT_NAME := cm_roamer
+#
+# Setup device specific product configuration.
+#
+PRODUCT_NAME := cyanogen_roamer
 PRODUCT_BRAND := zte
+PRODUCT_DEVICE := roamer
 PRODUCT_MODEL := roamer
 PRODUCT_MANUFACTURER := zte
+
+# Release name and versioning
+PRODUCT_RELEASE_NAME := N760
+PRODUCT_VERSION_DEVICE_SPECIFIC :=
+-include vendor/cyanogen/products/common_versions.mk
+
+#
+# Copy  specific prebuilt files
+#
+PRODUCT_COPY_FILES +=  \
+    vendor/cyanogen/prebuilt/hdpi/media/bootanimation.zip:system/media/bootanimation.zip
